e510146de4dd79a92c70ccc49308e55aa9839f5c
LynnePlex <kronixpirate@gmail.com>
2024-05-30 11:30:51 -0500

    score code change on main scene

--- scripts/main_game.gd
+++ scripts/main_game.gd
@@ -8,23 +8,32 @@ var score_right = 0
 @onready var ball = $ball
 @onready var score_left_label = $CanvasLayer/ScoreLeft
 @onready var score_right_label = $CanvasLayer/ScoreRight
+@onready var left_goal = $LeftGoal
+@onready var right_goal = $RightGoal
 
 
-func _ready():
-	score_left_label.text = str(score_left)
-	score_right_label.text = str(score_right)
+func _ready() -> void:
+	score_left_label = str(score_left)
+	score_right_label = str(score_right)
 	if ball == null:
 		ball = ball_scene
 		add_child(ball)
-	ball.connect("body_entered", Callable(self, "_on_ball_entered"))
+	left_goal.connect("body_entered", Callable(self, "_on_left_goal_body_entered"))
+	right_goal.connect("body_entered", Callable(self, "_on_right_goal_body_entered"))
 	
-func _on_ball_entered(body):
-	if body.global_position.x < 0:
+func _on_left_goal_body_entered(body: Node2D) -> void:
+	if body == ball:
 		score_right += 1
 		score_right_label = str(score_right)
-	else:
+		reset_ball()
+		
+func _on_right_goal_body_entered(body: Node2D) -> void:
+	if ball == ball:
 		score_left += 1
 		score_left_label = str(score_left)
+		reset_ball()
+		
+func reset_ball() -> void:
 	#reset ball position
 	ball.position = Vector2.ZERO
 	ball.linear_velocity = Vector2.RIGHT.rotated(randf() * PI * 2) * ball.speed
--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 @export var speed = 200
 
 func _process(_delta):
-	if Input.is_action_just_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-	if Input.is_action_just_pressed("ui_down"):
+	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
 	velocity = velocity.normalized() * speed
 	move_and_slide()
