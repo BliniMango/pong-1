f846954dd74cd8344147222538c6e8cfc6fba97e
LynnePlex <38146775+LynnePLex@users.noreply.github.com>
2024-05-30 10:14:26 -0500

    added the ball
    coded the ball

---
+++ scripts/ball.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+@export var speed = 300
+
+func _ready() -> void:
+	#add an impluse to the ball on start to get it bouncing
+	apply_impulse(Vector2.ZERO, Vector2.RIGHT.rotated(randf() * PI * 2) * speed)
+
+func _integrate_forces(state):
+	var velocity = state.get_linear_velocity()
+	velocity = velocity.normalized() * speed
+	state.linear_velocity()
--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-var speed = 200
+@export var speed = 200
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_up"):
