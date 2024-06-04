779a58aa90b654f2ddf04f8ebc78fcd8def201d0
LynnePlex <kronixpirate@gmail.com>
2024-06-03 21:37:08 -0500

    Messed around with boarder settings
    ball still no bouncy

--- scripts/Ball.gd
+++ scripts/Ball.gd
@@ -7,8 +7,8 @@ func _ready():
 	linear_velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized() * speed
 
 func _integrate_forces(state):
-	if position.y < 0 or position.y > 600:
+	if position.y < 0 or position.y > 650:
 		linear_velocity.y *= -1
 	
-	if position.x < 0 or position.x > 800:  # Assuming screen width is 800
+	if position.x < 0 or position.x > 1155:  # Assuming screen width is 800
 		linear_velocity.x *= -1
