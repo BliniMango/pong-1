cf9727dd5d8307c5b5bfe9186a9b7856dc70466b
LynnePlex <kronixpirate@gmail.com>
2024-06-04 03:30:02 -0500

    Paddle collision fix
    Revamp the collision for the AI and Player paddle
    
    Changed AI paddle to static body 2d

--- scripts/cpu_paddle.gd
+++ scripts/cpu_paddle.gd
@@ -1,9 +1,10 @@
 extends StaticBody2D
 
-@export var speed: float = 300
+@export var speed: float = 400
 @export var ball_path: NodePath
 
 var ball: CharacterBody2D
+var direction
 
 func _ready():
 	ball = get_node(ball_path)
@@ -15,3 +16,7 @@ func _process(delta):
 		position.y -= speed * delta
 	# to ensure the paddle stays within bounds
 	position.y = clamp(position.y, 0, 600 - $Sprite2D.texture.get_height())
+
+# bounces ball off cpu paddle
+func _on_area_2d_body_entered(body):
+	body.direction.x *= -1
--- scripts/player_paddle.gd
+++ scripts/player_paddle.gd
@@ -1,7 +1,11 @@
-extends StaticBody2D
+extends CharacterBody2D
 
 @export var speed = 300
 
+var direction
+
+
+# controls
 func _process(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_up"):
@@ -14,3 +18,7 @@ func _process(delta):
 	# make sure paddle stay in bounds
 	position.y = clamp(position.y, 0, 600 -
 	$Sprite2D.texture.get_height())
+
+# bounces ball off paddle
+func _on_area_2d_body_entered(body):
+	body.direction.x *= -1
